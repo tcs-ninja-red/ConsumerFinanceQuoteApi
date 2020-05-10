80a772e1b2872ac4ab8a99df54c90c62e826c9e4
vglkannan <35820063+vglkannan@users.noreply.github.com>
2020-05-10 15:21:35 +0100

    Make, Model Description API Issue resolve

--- src/constants/constants.common.js
+++ src/constants/constants.common.js
@@ -2,8 +2,8 @@
  * Common constants across all the environments (dev, staging, prod)
  */
 process.env.NODE_ENV = "dev";
-process.env.PORT = "3000";
+//process.env.PORT = "44301";
 module.exports = {
 	env: process.env.NODE_ENV,
-	port: process.env.PORT,
+	port: process.env.PORT ||44301,
 };
