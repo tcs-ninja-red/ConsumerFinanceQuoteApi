bfd72d60004af7d6e19e1ede4569c1f385c39928
vglkannan <35820063+vglkannan@users.noreply.github.com>
2020-07-13 22:20:12 +0100

    Enchanced proposal APIs
    Added Bank Account objects in proposal
    Added Employment details in proposal
    Proposal decision rule implemented
    Created addtional proposal test scripts

--- src/api/controllers/proposal.controller.js
+++ src/api/controllers/proposal.controller.js
@@ -17,20 +17,41 @@ exports.createProposal = async (req, res, next) => {
 
         const proposal = new Proposal({
             _id: new mongoose.Types.ObjectId(),
-            dealer_id: headers['dealer_id'],
-            decision: "ACCEPTED",
+            dealer_id: chunk.dealer_id,
+            decision: {
+                decision_status: null,
+                decision_message: null
+            },
             customer: {
-                first_name: chunk.customer.first_name,
+                title: chunk.customer.title,
+                fore_name: chunk.customer.fore_name,
+                middle_name: chunk.customer.middle_name,
                 surname: chunk.customer.surname,
                 date_of_birth: chunk.customer.date_of_birth,
                 email: chunk.customer.email,
+                phone: chunk.customer.phone,
+                gender: chunk.customer.gender,
+                marital_status: chunk.customer.marital_status,
+                country_of_origin: chunk.customer.country_of_origin,
                 address: {
                     address1: chunk.customer.address.address1,
                     address2: chunk.customer.address.address2,
                     address3: chunk.customer.address.address3,
                     postcode: chunk.customer.address.postcode,
                     town: chunk.customer.address.town,
-                    city: chunk.customer.address.city
+                    city: chunk.customer.address.city,
+                    time_at_address: chunk.customer.address.time_at_address
+                },
+                employment: {
+                    occupation: chunk.customer.employment.occupation,
+                    years_at_employment: chunk.customer.employment.years_at_employment,
+                    months_at_employment: chunk.customer.employment.months_at_employment,
+                    gross_annual_salary: chunk.customer.employment.gross_annual_salary
+                },
+                bank_account: {
+                    account_name: chunk.customer.bank_account.account_name,
+                    account_number: chunk.customer.bank_account.account_number,
+                    sort_code: chunk.customer.bank_account.sort_code
                 }
             },
             financial: {
@@ -71,26 +92,53 @@ exports.createProposal = async (req, res, next) => {
             }
         }
 
+        //Decision Rule
+        //Rejected: if customer age is less than 16
+        //Rejected: if monthly payment is greater than employment monthly salary
+        //Referred : if time at address is less than 36 months
+
+        var birthdate = proposal.customer.date_of_birth;
+        var cur = new Date();
+        var diff = cur-birthdate; // This is the difference in milliseconds
+        var age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25)); // Divide by 1000*60*60*24*365.25
+        
+        console.log('Age: ' + age);
+        console.log('Monthly salary: ' + proposal.customer.employment.gross_annual_salary / 12);
+
+        if (proposal.customer.address.time_at_address < 36) {
+            proposal.decision.decision_status = "REFERRED";
+            proposal.decision.decision_message = "Proposal has been Referred due to given time at address is lesser than minimum time at address allowed.";
+        }
+        else if (age < 16) {
+            proposal.decision.decision_status = "REJECTED";
+            proposal.decision.decision_message = "Proposal has been Rejected due to given customer's age is lesser than minimum age allowed.";
+        }
+        else if (proposal.customer.employment.gross_annual_salary / 12 <= proposal.monthly_payment_amount) {
+            proposal.decision.decision_status = "REJECTED";
+            proposal.decision.decision_message = "Proposal has been rejected due to given customer's monthly salary is lesser than monthly payment amount allowed.";
+        }
+        else {
+            proposal.decision.decision_status = "ACCEPTED";
+            proposal.decision.decision_message = "Proposal has been accepted.";
+        }
+
         proposal.save().then(result => {
             //console.log(result);
             res.status(httpStatus.CREATED).json({
-                message: "Proposal Created Successfully",
-                decision: {
-                    proposal_ref_number: result._id,
-                    message: result.decision,
-                    request: [
-                        {
-                            type: "GET",
-                            description: "Get proposal for the given proposal Id",
-                            url: "http://localhost:44301/api/v1/proposals/" + result._id
-                        },
-                        {
-                            type: "GET",
-                            description: "Get all proposals associated with the dealer",
-                            url: "http://localhost:44301/api/v1/proposals/"
-                        }
-                    ]
-                }
+                proposal_ref_number: result._id,
+                decision: result.decision,
+                request: [
+                    {
+                        type: "GET",
+                        description: "Get proposal for the given proposal Id",
+                        url: req.protocol + "://" + req.headers.host  +  req.originalUrl + "/" + result._id
+                    },
+                    {
+                        type: "GET",
+                        description: "Get all proposals associated with the dealer",
+                        url: req.protocol + "://" + req.headers.host  +  req.originalUrl
+                    }
+                ]
             });
         }).catch(err => {
             console.log(err.message);
